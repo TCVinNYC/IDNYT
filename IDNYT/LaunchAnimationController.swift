3b394b8fe4361a7acc52463b4624060c79945a05
Stevenson Chittumuri <stevensonchittumuri@gmail.com>
2021-11-01 01:06:09 -0400

    Added more ViewControllers and some LaunchScreen animation

---
+++ IDNYT/LaunchAnimationController.swift
@@ -0,0 +1,46 @@
+//
+//  LaunchAnimationController.swift
+//  IDNYT
+//
+//  Created by Prince on 10/30/21.
+//
+
+import UIKit
+
+class LaunchAnimationController: UIViewController {
+    
+    @IBOutlet weak var nyitIcon: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        nyitIcon.alpha = 0
+        animate()
+    }
+    
+    private func animate(){
+        self.nyitIcon.alpha = 1
+        
+        /// idk how to do the bounce thing but fix later?
+        
+        UIView.animate(withDuration: 0.5){
+           //self.nyitIcon.frame = CGRect(x: 0, y: 0, width: self.view.frame.width+25, height: self.view.frame.height+25)
+            //print("big")
+            
+            self.nyitIcon.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+            print("normal")
+            
+        } completion: { done in
+            if done {
+                //later we will see if the user is logged in
+                //for now just default to log in screen
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2){
+                    self.performSegue(withIdentifier: "LoginScreen", sender: self)
+                    print("moving to login")
+                }
+
+            }
+        }
+
+    }
+}
